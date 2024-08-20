a28f5b46fb009e43cc6816f37d3f1548784df50f
AlanLFM <113216725+AlanLFM@users.noreply.github.com>
2024-08-20 00:58:27 -0600

    Añado donador y user

---
+++ models/Donador.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const ONGSchema = new mongoose.Schema({
+  organization: { type: String, required: true },
+  description: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
+});
+
+// Especifica el nombre de la colección como 'register'
+module.exports = mongoose.model('ONG', ONGSchema, 'register');
